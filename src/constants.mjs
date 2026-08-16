c77542c8be1f825dab48b8083a52833ea97a0b2f
kevynlucasprofissional-stack <kevynlucasprofissional@gmail.com>
2026-08-16 00:26:33 -0300

    add course-specific skip policy

--- src/constants.mjs
+++ src/constants.mjs
@@ -4,11 +4,22 @@ export const MATERIALS_PATH = '/api/materials/download';
 export const LESSON_URL_RE = /^https:\/\/www\.xcursos\.com\/curso\/[^/]+\/aula\/[^/?#]+/i;
 
 export const TERMINAL_STATUSES = new Set([
-  'DOWNLOADED','ALREADY_PRESENT','NO_VIDEO','DRM_PROTECTED',
+  'DOWNLOADED','ALREADY_PRESENT','NO_VIDEO','DRM_PROTECTED','SKIPPED',
   'DOWNLOAD_FAILED','VERIFY_FAILED','MEDIA_NOT_FOUND','MEDIA_NOT_READY',
 ]);
 export const FILE_BACKED_STATUSES = new Set(['DOWNLOADED','ALREADY_PRESENT']);
 export const RETRYABLE_FAILURE_STATUSES = new Set(['DOWNLOAD_FAILED','VERIFY_FAILED','MEDIA_NOT_FOUND','MEDIA_NOT_READY']);
+
+export const LESSON_SKIP_POLICIES = Object.freeze([
+  Object.freeze({
+    id:'vtsd-2026-audio-ads-106-123',
+    courseName:'VENDA TODO SANTO DIA 2026 - LEANDRO LADEIRA',
+    totalPositions:198,
+    ranges:Object.freeze([Object.freeze({start:106,end:123})]),
+    reason:'USER_EXCLUDED_AD_AUDIO_BLOCK',
+    label:'Propagandas em áudio',
+  }),
+]);
 
 export const DEFAULT_LIMITS = Object.freeze({
   browserLaunchTimeoutMs: 30_000,
